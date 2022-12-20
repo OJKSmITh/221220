ecf895d4ebd04b64cd398d9cc4bdaddaa3c7ccb5
Hanjin <csa0545@gamil.com>
2022-12-20 09:24:13 +0900

    가짜 db 설정

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ nunjucks.configure("views", {
     express: app,
 })
 
+const items = [{
+    subject: "첫번쨰 게시물",
+    content: "content",
+    name: "name",
+},
+]
+
 app.get('/', (req, res) => {
     const { name } = req.query
     res.render("index.html", { name })
@@ -14,19 +21,19 @@ app.get('/', (req, res) => {
 
 app.get('/list', (req, res) => {
     res.render("board/list.html")
-})
+}) // 내가 데이터를 어떠한 형태로 만들래, 일단 가짜 db할것
 
 app.get('/write', (req, res) => {
     res.render('board/write.html')
-})
+}) // 특정 데이터를 어떻게 만들어 줄래
 
 app.get('/view', (req, res) => {
     res.render('board/view.html')
-})
+}) // 특정 데이터 어떻게 보여줄래
 
 app.get('/modify', (req, res) => {
     res.render("board/modify.html")
-})
+}) // view + write가 합쳐진것
 
 app.listen(3000, (req, res) => {
     console.log("서버열림!")
