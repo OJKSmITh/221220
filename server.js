b63c80689da023253887c1076548b96046f5870e
Hanjin <csa0545@gamil.com>
2022-12-20 09:19:39 +0900

    전체 html연결

--- server.js
+++ server.js
@@ -16,6 +16,18 @@ app.get('/list', (req, res) => {
     res.render("board/list.html")
 })
 
+app.get('/write', (req, res) => {
+    res.render('board/write.html')
+})
+
+app.get('/view', (req, res) => {
+    res.render('board/view.html')
+})
+
+app.get('/modify', (req, res) => {
+    res.render("board/modify.html")
+})
+
 app.listen(3000, (req, res) => {
     console.log("서버열림!")
 })
